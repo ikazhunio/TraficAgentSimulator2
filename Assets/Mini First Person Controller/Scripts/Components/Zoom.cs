aed57ca530f4511a11a2814469b59c635d0c06b0
Ika Zhunio <ika.zhunio@est.ucacue.edu.ec>
2026-05-30 03:12:41 -0500

    FirstCharacterController
    Integracion de sistema de movimiento y autosave

---
+++ Assets/Mini First Person Controller/Scripts/Components/Zoom.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[ExecuteInEditMode]
+public class Zoom : MonoBehaviour
+{
+    Camera camera;
+    public float defaultFOV = 60;
+    public float maxZoomFOV = 15;
+    [Range(0, 1)]
+    public float currentZoom;
+    public float sensitivity = 1;
+
+
+    void Awake()
+    {
+        // Get the camera on this gameObject and the defaultZoom.
+        camera = GetComponent<Camera>();
+        if (camera)
+        {
+            defaultFOV = camera.fieldOfView;
+        }
+    }
+
+    void Update()
+    {
+        // Update the currentZoom and the camera's fieldOfView.
+        float scroll = Mouse.current.scroll.ReadValue().y;
+        currentZoom = Mathf.Clamp01(currentZoom);
+        camera.fieldOfView = Mathf.Lerp(defaultFOV, maxZoomFOV, currentZoom);
+    }
+}
